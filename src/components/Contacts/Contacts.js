5a23a21cd032feedd9b875a32b54c93e6b8e037a
azinoVo <azinovo22@gmail.com>
2019-12-16 18:53:58 -0600

    added styling for contacts

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import './Contacts.css';
 
 const Contacts = () => {
     return (
         <div className='contacts'>
-            <a href="https://www.linkedin.com/in/nguyen-vo22/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className='fa-icon' icon={faLinkedin} size="2x"/></a>
-            <a href="https://github.com/azinoVo" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className='fa-icon' icon={faGithub} size="2x" /></a>
+            <span className='contact'><a href="https://www.linkedin.com/in/nguyen-vo22/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className='fa-icon' icon={faLinkedin} size="2x"/></a></span>
+            <span className='contact'><a href="https://github.com/azinoVo" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className='fa-icon' icon={faGithub} size="2x" /></a></span>
         </div>
     );
 }
