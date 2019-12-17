c13c9d9ac06b6fcfb34387cf68ef33689d2fbaba
azinoVo <azinovo22@gmail.com>
2019-12-17 16:56:11 -0600

    added resume icon

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import resume from '../../assets/_ Resume for Nguyen Vo [Version 2].pdf'
 import './Contacts.css';
 
 const Contacts = () => {
@@ -8,6 +10,7 @@ const Contacts = () => {
         <div className='contacts'>
             <span className='contact'><a href="https://www.linkedin.com/in/nguyen-vo22/" target="_blank" rel="noopener noreferrer" title="LinkedIn"><FontAwesomeIcon className='fa-icon' icon={faLinkedin} size="2x"/></a></span>
             <span className='contact'><a href="https://github.com/azinoVo" target="_blank" rel="noopener noreferrer" title="GitHub"><FontAwesomeIcon className='fa-icon' icon={faGithub} size="2x" /></a></span>
+            <span className='contact'><a href={resume} target="_blank" title="Download Resume"><FontAwesomeIcon className='fa-icon' icon={faFile} size="2x"/></a></span>
         </div>
     );
 }
