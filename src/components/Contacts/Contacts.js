c5a52658db00e8d6267bf72f5d9a82db5ffad33b
azinoVo <azinovo22@gmail.com>
2019-12-17 16:20:28 -0600

    added title tags

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -6,8 +6,8 @@ import './Contacts.css';
 const Contacts = () => {
     return (
         <div className='contacts'>
-            <span className='contact'><a href="https://www.linkedin.com/in/nguyen-vo22/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className='fa-icon' icon={faLinkedin} size="2x"/></a></span>
-            <span className='contact'><a href="https://github.com/azinoVo" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className='fa-icon' icon={faGithub} size="2x" /></a></span>
+            <span className='contact'><a href="https://www.linkedin.com/in/nguyen-vo22/" target="_blank" rel="noopener noreferrer" title="LinkedIn"><FontAwesomeIcon className='fa-icon' icon={faLinkedin} size="2x"/></a></span>
+            <span className='contact'><a href="https://github.com/azinoVo" target="_blank" rel="noopener noreferrer" title="GitHub"><FontAwesomeIcon className='fa-icon' icon={faGithub} size="2x" /></a></span>
         </div>
     );
 }
