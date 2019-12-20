39f897e321a03f1943f78c452b245968da1ba56b
azinoVo <azinovo22@gmail.com>
2019-12-20 16:33:10 -0600

    added more to bio

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,8 +10,10 @@ const AboutMe = () => {
         <div className='bioWrapper'>
             <p>Hello, friends! I'm a developer originally from Vietnam. I 
                 immigrated to the United States in 2001 and have made
-                the Big Easy my new home. When I'm not debugging, 
-                I'm usually reading fantasy books, writing stories, playing League
+                the Big Easy my new home. With a background in science and education,
+                I have strong problem-solving, communication, procedural, and design skills.
+                I hope to use these skills creatively to design and create amazing products.
+                When I'm not debugging, I'm usually reading fantasy books, writing stories, playing League
                 of Legends, or dragging my friends out to grab sushi. To view my 
                 projects, scroll below or click the handy dandy arrow.
             </p>
