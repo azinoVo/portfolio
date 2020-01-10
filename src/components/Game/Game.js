ccf0a568814504980c3db30e5969288823708665
azinoVo <azinovo22@gmail.com>
2020-01-09 18:21:59 -0600

    added folder for game components-idea in progress

---
+++ src/components/Game/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Game = () => {
+    return (
+        <div>
+            THIS IS THE GAME! It will be a semi-raid boss who will do skills and such depending on
+            certain situations. Users can create a character and help to take down the raid boss.
+        </div>
+    );
+}
+
+export default Game;
