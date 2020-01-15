298c857150433adddb8c468bf7438f5cf2da5617
azinoVo <azinovo22@gmail.com>
2020-01-14 19:50:54 -0600

    added the slime boss

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,13 +4,14 @@ const Game = () => {
     return (
         <>
         <div className='boss'>
-            This will be where the raid boss will be placed along with its health and any status effects on it.
+            <img src={require('../../assets/Slime.gif')} alt="loading..." />
+            {/* This will be where the raid boss will be placed along with its health and any status effects on it. */}
         </div>
         <div className='user-info'>
-            This will store user information such as health from database and skills that the class has.
+            {/* This will store user information such as health from database and skills that the class has. */}
         </div>
         <div className='general-info'>
-            This will display game dialogue.
+            {/* This will display game dialogue. */}
         </div>
         </>
 
